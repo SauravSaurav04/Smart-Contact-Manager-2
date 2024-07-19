905f4b5ec1c5e4d3759e462ae39c45f555729b46
SauravSaurav04 <sauravsaurav4444@gmail.com>
2024-07-19 20:38:29 +0530

    fragment setup

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -11,11 +11,16 @@ public class PageController {
     public String home(Model model){
         System.out.println("Home page handler");
 
-        // sending data to view
         model.addAttribute("name", "Substring Technologies");
         model.addAttribute("youtubeChannel", "Learn Code With Durgesh");
         model.addAttribute("githubRepo", "https://github.com/learncodewithdurgesh/");
 
         return "home";
+    }
+
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        System.out.println("About page handler");
+        return "about";
     }
 }
