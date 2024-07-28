e4e3d49f407fce4ecca51e58309e15591df88a6b
SauravSaurav04 <sauravsaurav4444@gmail.com>
2024-07-28 14:04:51 +0530

    View particular contact implementation done

--- src/main/java/com/scm/entities/Contact.java
+++ src/main/java/com/scm/entities/Contact.java
@@ -1,5 +1,6 @@
 package com.scm.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -31,6 +32,7 @@ public class Contact {
     public String cloudinaryImagePublicId;
 
     @ManyToOne
+    @JsonIgnore
     private User user;
 
     @OneToMany(mappedBy = "contact", cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
